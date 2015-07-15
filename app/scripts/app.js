a76447be4b3684d0fb30ac18240adbae36d62552
Victor Perron <victor.perron@polyconseil.fr>
2015-07-15 09:48:23 +0200

    __WIP

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -61,8 +61,10 @@ angular.module('app', [
   }
 
   var element = document.getElementById('image');
+  element.style.display = 'none'; // hide the image source for now
   var ctx = element.getContext('2d');
 
+  var freq = 2;
   var img = new Image();
   img.src = 'assets/img.jpg';
   img.onload = function() {
@@ -73,22 +75,64 @@ angular.module('app', [
     ctx.canvas.width = w;
     ctx.canvas.height = h;
     
-    ctx.drawImage(img, 0, 0);
+    ctx.drawImage(img, 0, 0);  // draw it once, to get the Image Data
     var imageData = ctx.getImageData(0, 0, w, h);
     
     ctx.fillStyle = '#dedede';
     ctx.fillRect(0, 0, w, h);
 
-    var origData = new Uint8ClampedArray(imageData.data);
-
     // Corners
     var divider = h / 20;
     var layer = ctx.createImageData(w, divider);
 
-    for(var u=0; u<h/divider; u=u+1) {
-      var pixels = imageData.data.subarray(u*divider*w*4, (u+1)*divider*w*4);
-      layer.data.set(pixels);
-      ctx.putImageData(layer, 0, u*divider);
+    // Canvasses
+    var children = document.getElementById('children');
+    var images = new Array(divider);
+    for(var i=0; i<divider; i=i+1) {
+      var canvas = document.createElement('canvas');
+      children.appendChild(canvas);
+      var context = canvas.getContext('2d');
+      images[i] = {
+        context: context,
+        imageData: context.createImageData(w, divider),
+      };
+    };
+
+    var n = 0;
+    function redraw() {
+      console.log('redrawing...');
+      var index = 0;
+      var pixels = imageData.data.subarray(index*divider*w*4, (index+1)*divider*w*4);
+      // layer.data.set(pixels);
+      // ctx.putImageData(layer, 0, index*divider);
+
+      // Subcanvasses
+      var item = images[index];
+      if (n % freq) {
+        item.imageData.data.set(pixels);
+        item.context.canvas.width = w;
+        item.context.canvas.height = divider;
+        item.context.putImageData(item.imageData, 0, 0);
+      } else {
+        item.context.fillStyle = '#dedede';
+        item.context.fillRect(0, 0, w, divider);
+      }
+      n = n + 1;
+      for(var index=1; index<h/divider; index=index+1) {
+        var pixels = imageData.data.subarray(index*divider*w*4, (index+1)*divider*w*4);
+        // layer.data.set(pixels);
+        // ctx.putImageData(layer, 0, index*divider);
+
+        // Subcanvasses
+        var item = images[index];
+        item.imageData.data.set(pixels);
+        item.context.canvas.width = w;
+        item.context.canvas.height = divider;
+        item.context.putImageData(item.imageData, 0, 0);
+      }
     }
+
+    redraw();
+    setInterval(redraw, 1000/freq);
   };
 });
